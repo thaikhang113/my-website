927c526d89b44e621502f1ff9fbcebd11d99b014
thaikhang113 <cvcaothaikhang29@gmail.com>
2025-10-02 14:54:05 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,13 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('D:/www'));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello from Express!</h1><p>This is dynamic content 🚀</p>');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
